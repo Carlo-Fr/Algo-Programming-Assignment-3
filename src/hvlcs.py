f062c708694c3119267f1d4ce08934105f08c42a
Kavipatel0 <kavipatel@ufl.edu>
2026-04-03 23:16:37 -0400

    Implemented DP table and calculated max value

--- src/hvlcs.py
+++ src/hvlcs.py
@@ -21,11 +21,31 @@ def read_input():
 
     return alphabet_values, string_a, string_b
 
+def build_dp_table(alphabet_values, string_a, string_b):
+    n = len(string_a)
+    m = len(string_b)
+
+    dp = [[0] * (m + 1) for _ in range(n+1)]
+
+    for i in range(1,n+1):
+        for j in range(1, m+1):
+            if string_a[i-1] == string_b[j-1]:
+                #Characters Match
+                char_val = alphabet_values[string_a[i - 1]]
+                dp[i][j] = dp[i-1][j-1] + char_val
+            else:
+                #Mismatch
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+    return dp
+
 def main():
     alphabet_values, string_a, string_b = read_input()
     
     if not alphabet_values:
         return
+
+    
+
 
     # test statements to check parsing
     print(f"Alphabet Values: {alphabet_values}")
@@ -33,6 +53,12 @@ def main():
     print(f"String B: {string_b}")
 
     # todo - implement DP table using variables
+
+    #Building the table
+    dp_table = build_dp_table(alphabet_values, string_a, string_b)
+    #Get Max Value
+    max_value = dp_table[-1][-1]
+    print(f"Maximum Value: {max_value}")
 
 if __name__ == "__main__":
     main()
